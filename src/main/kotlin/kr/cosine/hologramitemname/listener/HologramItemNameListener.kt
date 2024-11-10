7ad35f6309858e53f7042b415c9fd61970475683
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2024-11-10 11:34:32 +0900

    fix: 아이템을 버릴 때 개수가 증가하지 않는 오류 해결

--- src/main/kotlin/kr/cosine/hologramitemname/listener/HologramItemNameListener.kt
+++ src/main/kotlin/kr/cosine/hologramitemname/listener/HologramItemNameListener.kt
@@ -1,26 +1,44 @@
 package kr.cosine.hologramitemname.listener
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kr.cosine.hologramitemname.config.SettingConfig
+import kr.hqservice.framework.bukkit.core.HQBukkitPlugin
+import kr.hqservice.framework.bukkit.core.coroutine.extension.BukkitMain
+import kr.hqservice.framework.bukkit.core.listener.HandleOrder
 import kr.hqservice.framework.bukkit.core.listener.Listener
 import kr.hqservice.framework.bukkit.core.listener.Subscribe
 import kr.hqservice.framework.nms.extension.getDisplayName
 import org.bukkit.entity.Item
-import org.bukkit.event.entity.EntitySpawnEvent
+import org.bukkit.event.entity.ItemMergeEvent
+import org.bukkit.event.entity.ItemSpawnEvent
 
 @Listener
 class HologramItemNameListener(
+    private val plugin: HQBukkitPlugin,
     private val settingConfig: SettingConfig
 ) {
-    @Subscribe
-    fun onItemSpawn(event: EntitySpawnEvent) {
+    @Subscribe(HandleOrder.LAST)
+    fun onItemSpawn(event: ItemSpawnEvent) {
+        if (event.isCancelled) return
+        event.entity.applyHologramItemName()
+    }
+
+    @Subscribe(HandleOrder.LAST)
+    fun onItemMerge(event: ItemMergeEvent) {
+        if (event.isCancelled) return
+        plugin.launch(Dispatchers.BukkitMain) {
+            event.target.applyHologramItemName()
+        }
+    }
+
+    private fun Item.applyHologramItemName(amount: Int? = null) {
         if (!settingConfig.isEnabled) return
-        val item = event.entity as? Item ?: return
-        val itemStack = item.itemStack
         val hologramFormat = settingConfig.getHologramFormat {
             it.replace("%item%", itemStack.getDisplayName())
-                .replace("%amount%", "${itemStack.amount}")
+                .replace("%amount%", "${amount ?: itemStack.amount}")
         }
-        item.isCustomNameVisible = true
-        item.customName = hologramFormat
+        customName = hologramFormat
+        isCustomNameVisible = true
     }
 }
